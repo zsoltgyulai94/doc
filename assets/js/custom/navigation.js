27b155002e4cdfd657e115e60a2f20cd11b4f203
Hofi <hofione@gmail.com>
2024-02-04 22:28:35 +0100

    Added navigation that does not reload the navigation linked target pages, just loads them into the right content panel
    Sidebar navigator now should sync its active item, open it up and select automatically according to the active content in the right content panel and url
    Signed-off-by: Hofi <hofione@gmail.com>

---
+++ assets/js/custom/navigation.js
@@ -0,0 +1,113 @@
+
+function adjustSidebar() {
+  // Identify the URL of the loaded page
+  var loadedPageUrl = window.location.pathname;
+
+  // Get the "sidebar" element
+  var sidebarElement = document.querySelector('.sidebar');
+  // If "sidebar" element exists, find the corresponding navigator item within it
+  if (sidebarElement) {
+    // Get all <a> elements in the navigation list within the "sidebar"
+    var navLinks = sidebarElement.querySelectorAll('.nav__list .nav-link');
+
+    // Enumerate through each <a> element and remove the "active" class
+    navLinks.forEach(navLink => {
+      navLink.classList.remove('active');
+    });
+
+    // Find the corresponding navigator item with matching URL within the "sidebar"
+    var matchingNavItem = sidebarElement.querySelector('.nav__list .nav-link[href="' + loadedPageUrl + '"]');
+
+    // If a matching item is found, mark it as "active"
+    if (matchingNavItem) {
+      matchingNavItem.classList.add('active');
+
+      // Expand all parent ul elements up to the nearest .nav__list
+      var parentUl = matchingNavItem.closest('ul');
+      while (parentUl) {
+        var parentCheckbox = parentUl.previousElementSibling.previousElementSibling;
+        if (parentCheckbox && parentCheckbox.tagName === 'INPUT' && parentCheckbox.type === 'checkbox')
+          parentCheckbox.checked = true;
+
+        var immediateParent = parentUl.parentElement;
+        if (immediateParent.classList.contains('nav__list'))
+          break;
+
+        parentUl = immediateParent.closest('ul');
+      }
+
+      // Ensure the active item is visible within the "sidebar"
+      // TODO: This one is also not too reliable, browser dependant,get a better solution
+      matchingNavItem.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    }
+  }
+}
+
+// Function to load content based on relative URL
+function loadContentFromUrl(url) {
+  var currContentContainer = document.querySelector('article');
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Page not found');
+      }
+      return response.text();
+    })
+    .then(html => {
+      // Parse the HTML string to create a DOM document
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(html, 'text/html');
+      // Find the "article" element in the parsed document
+      var newContent = doc.querySelector('article');
+
+      // FIXME: This does not work, double check
+      currContentContainer.scrollTop;
+
+      // As a workaround of the above, empty the old content, and with a short delay only, load the new one
+      currContentContainer.innerHTML = '';
+
+      // Replace the old content, but only with a small delay, to make sure the content reset takes effect
+      setTimeout(function () {
+        // Replace the old content with the loaded content
+        currContentContainer.parentNode.replaceChild(newContent, currContentContainer);
+        // Sync the sidebar with the current page url, migth be out of sync when the page is loaded initially from an inner url
+        adjustSidebar();
+        // There might be nav-links in the loaded new content as well (e.g.Next / Prev buttons
+        // so, handle the links here as the last action
+        updateNavLinks();
+      }, 100);
+    })
+    .catch(error => {
+      contentContainer.innerHTML = '<h2>Error loading content</h2>';
+    });
+}
+
+// Function to handle link clicks
+function handleLinkClick(event) {
+  event.preventDefault(); // Prevent default navigation behavior
+
+  // Get the relative URL value and update the browser URL
+  var url = new URL(event.target.href).pathname;
+  history.pushState(null, null, url);
+
+  // Load content based on the updated relative URL
+  loadContentFromUrl(url);
+}
+
+function updateNavLinks(event) {
+  // Attach click event listeners to all links with class 'nav-link'
+  var navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', handleLinkClick);
+  });
+}
+
+// Initial load based on the relative URL if needed 
+// (e.g. when an inner embedded page link is opened directly in a new tab)
+loadContentFromUrl(window.location.pathname);
+
+// Listen for popstate events and update content accordingly
+window.addEventListener('popstate', function () {
+  loadContentFromUrl(window.location.pathname);
+});
